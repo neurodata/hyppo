6b96375ebdd2941cf82f451c67ddc95a79788ff6
Sambit Panda <sampanda501@gmail.com>
2019-10-10 18:37:26 -0400

    add relative import code to __init__.py

---
+++ mgc/__init__.py
@@ -0,0 +1,5 @@
+import mgc.independence
+import mgc.ksample
+import mgc.time_series
+
+__version__ = "0.0.1"
---
+++ mgc/independence/__init__.py
@@ -0,0 +1,5 @@
+from .pearson import Pearson
+from .rvcorr import RVCorr
+from .cca import CannCorr
+
+__all__ = ["Pearson", "RVCorr", "CannCorr"]
