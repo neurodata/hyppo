4cf0b872266f93b3e28b57b9c6199393b251e76e
Sambit Panda <sampanda501@gmail.com>
2019-10-29 12:30:39 -0400

    fix issues with error checking in time series

--- mgc/time_series/_utils.py
+++ mgc/time_series/_utils.py
@@ -16,12 +16,12 @@ class _CheckInputs:
         self.compute_distance = compute_distance
         self.reps = reps
 
-    def __call__(self, test_name):
+    def __call__(self):
         check_ndarray_xy(self.x, self.y)
         contains_nan(self.x)
         contains_nan(self.y)
         self.max_lag = self._check_max_lag()
-        self.x, self.y = self.check_dim_xy(test_name)
+        self.x, self.y = self.check_dim_xy()
         self.x, self.y = convert_xy_float64(self.x, self.y)
         self._check_min_samples()
         check_compute_distance(self.compute_distance)
@@ -31,7 +31,7 @@ class _CheckInputs:
 
         return self.x, self.y
 
-    def check_dim_xy(self, test_name):
+    def check_dim_xy(self):
         # check if x and y are ndarrays
         # convert arrays of type (n,) to (n, 1)
         if self.x.ndim == 1:
@@ -39,11 +39,11 @@ class _CheckInputs:
         if self.y.ndim == 1:
             self.y.shape = (-1, 1)
 
-        self._check_nd_indeptest(test_name)
+        self._check_nd_indeptest()
 
         return self.x, self.y
 
-    def _check_nd_indeptest(self, test_name):
+    def _check_nd_indeptest(self):
         nx = self.x.shape[0]
         ny = self.y.shape[0]
 
