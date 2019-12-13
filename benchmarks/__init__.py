9ccebf6c99ec5cb15ad2bc789aaa7e1387807eaa
Sambit Panda <sampanda501@gmail.com>
2019-12-13 12:12:04 -0500

    add 3 sample curves data

--- benchmarks/__init__.py
+++ benchmarks/__init__.py
@@ -1,3 +1,3 @@
-from .power import power_sample, power_dim
+from .power import power_sample, power_dim, power_3samp_epsilon
 
-__all__ = ["power_sample", "power_dim"]
+__all__ = ["power_sample", "power_dim", "power_3samp_epsilon"]
