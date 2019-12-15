8afe8bc6316059757ec759f4d2a8847bb75e64a5
Sambit Panda <sampanda501@gmail.com>
2019-12-15 08:51:52 -0500

    add sample size notebook and power curve

--- benchmarks/__init__.py
+++ benchmarks/__init__.py
@@ -1,4 +1,11 @@
 from .power import power_sample, power_dim
+from .power_2samp import power_2samp_sample, power_2samp_dimension
 from .power_3samp import power_3samp_epsilon
 
-__all__ = ["power_sample", "power_dim", "power_3samp_epsilon"]
+__all__ = [
+    "power_sample",
+    "power_dim",
+    "power_2samp_sample",
+    "power_2samp_dimension",
+    "power_3samp_epsilon",
+]
