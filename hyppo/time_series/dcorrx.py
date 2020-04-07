c76a5a2af9844b8f1d33d0bb98396c2eeec92de8
Ronak Mehta <rmehta004@gmail.com>
2020-04-06 21:20:14 -0400

    Fixed doc tests.

--- hyppo/time_series/dcorrx.py
+++ hyppo/time_series/dcorrx.py
@@ -160,7 +160,7 @@ class DcorrX(TimeSeriesTest):
         >>> dcorrx = DcorrX(compute_distance=None)
         >>> stat, pvalue, dcorrx_dict = dcorrx.test(x, y)
         >>> '%.1f, %.2f' % (stat, pvalue)
-        '0.0, 1.00'
+        '1.0, 1.00'
         """
         check_input = _CheckInputs(
             x, y, max_lag=self.max_lag, compute_distance=self.compute_distance
