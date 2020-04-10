443a8a3f7229521223f3b7c0ba9a1ae57d27104b
bvarjavand <bvarjavand@gmail.com>
2020-04-10 10:03:13 -0700

    black

--- hyppo/independence/base.py
+++ hyppo/independence/base.py
@@ -65,7 +65,9 @@ class IndependenceTest(ABC):
         self.y = y
 
         # calculate p-value
-        stat, pvalue, null_dist = perm_test(self._statistic, x, y, reps=reps, workers=workers)
+        stat, pvalue, null_dist = perm_test(
+            self._statistic, x, y, reps=reps, workers=workers
+        )
         self.stat = stat
         self.pvalue = pvalue
         self.null_dist = null_dist
