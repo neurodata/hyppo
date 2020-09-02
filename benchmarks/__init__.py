92ff0a5200998da5218352298bad7bbace5791ac
rflperry <rflperry@gmail.com>
2020-09-02 11:41:33 -0400

    removed 4samp, relative figures

--- benchmarks/__init__.py
+++ benchmarks/__init__.py
@@ -1,7 +1,6 @@
 from .power import power_sample, power_dim
 from .power_2samp import power_2samp_sample, power_2samp_dimension, power_2samp_angle
 from .power_3samp import power_3samp_epsweight
-from .power_4samp import power_4samp_epsweight
 from .power_4samp_2way import power_4samp_2way_epsweight
 
 __all__ = [
@@ -11,6 +10,5 @@ __all__ = [
     "power_2samp_dimension",
     "power_2samp_angle",
     "power_3samp_epsweight",
-    "power_4samp_epsweight",
     "power_4samp_2way_epsweight",
 ]
