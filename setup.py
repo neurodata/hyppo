74e7242ab380354d68ee0adc83ffd7e531b8a727
Sambit Panda <36676569+sampan501@users.noreply.github.com>
2021-11-10 13:33:26 -0500

    remove 3.10 reference

--- setup.py
+++ setup.py
@@ -72,7 +72,6 @@ setup(
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
-        "Programming Language :: Python :: 3.10",
     ],
     packages=find_packages(),
     include_package_data=True,
