a7ca20ee21d36e14131895a6a3f6dc03be56278b
darsh-patel <70541374+darsh-patel@users.noreply.github.com>
2021-12-11 14:21:13 -0500

    Rewrote necessary unit tests with pytest

--- hyppo/kgof/tests/test_density.py
+++ hyppo/kgof/tests/test_density.py
@@ -1,62 +1,59 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from numpy import testing
 
-from .. import data, density, _utils, kernel, fssd, h0simulator
+from .. import density
 import scipy.stats as stats
-
-import unittest
 from numpy.random import default_rng
 
+import unittest
+import pytest
 
 class TestIsotropicNormal(unittest.TestCase):
-
-    def test_log_den(self):
-        n = 7
+    @pytest.mark.parametrize("n", [7])
+    @pytest.mark.parametrize("d", [3, 1])
+    def test_log_den(self, n, d):
         rng = default_rng(16)
-        for d in [3, 1]:
-            variance = 1.1
-            mean = rng.standard_normal(size=d)
-            X = rng.random(size=(n, d)) + 1
+        variance = 1.1
+        mean = rng.standard_normal(size=d)
+        X = rng.random(size=(n, d)) + 1
 
-            isonorm = density.IsotropicNormal(mean, variance)
-            log_dens = isonorm.log_den(X)
-            my_log_dens = -np.sum((X - mean) ** 2, 1) / (2.0 * variance)
+        isonorm = density.IsotropicNormal(mean, variance)
+        log_dens = isonorm.log_den(X)
+        my_log_dens = -np.sum((X - mean) ** 2, 1) / (2.0 * variance)
 
-            # check correctness
-            np.testing.assert_almost_equal(log_dens, my_log_dens)
+        # check correctness
+        testing.assert_almost_equal(log_dens, my_log_dens)
 
-    def test_grad_log(self):
-        n = 8
+    @pytest.mark.parametrize("n", [8])
+    @pytest.mark.parametrize("d", [4, 1])
+    def test_grad_log(self, n, d):
         rng = default_rng(17)
-        for d in [4, 1]:
-            variance = 1.2
-            mean = rng.standard_normal(size=d) + 1
-            X = rng.random(size=(n, d)) - 2
+        variance = 1.2
+        mean = rng.standard_normal(size=d) + 1
+        X = rng.random(size=(n, d)) - 2
 
-            isonorm = density.IsotropicNormal(mean, variance)
-            grad_log = isonorm.grad_log(X)
-            my_grad_log = -(X - mean) / variance
+        isonorm = density.IsotropicNormal(mean, variance)
+        grad_log = isonorm.grad_log(X)
+        my_grad_log = -(X - mean) / variance
 
-            # check correctness
-            np.testing.assert_almost_equal(grad_log, my_grad_log)
-
-    def tearDown(self):
-        pass
+        # check correctness
+        testing.assert_almost_equal(grad_log, my_grad_log)
 
 
 class TestGaussianMixture(unittest.TestCase):
-    def test_multivariate_normal_density(self):
-        for i in range(4):
-            rng = default_rng(i + 8)
-            d = i + 2
-            cov = stats.wishart(df=10 + d, scale=np.eye(d)).rvs(size=1)
-            mean = rng.standard_normal(size=d)
-            X = rng.standard_normal(size=(11, d))
-            den_estimate = density.GaussianMixture.multivariate_normal_density(
-                mean, cov, X
-            )
+    @pytest.mark.parametrize("i", [0, 1, 2, 3])
+    def test_multivariate_normal_density(self, i):
+        rng = default_rng(i + 8)
+        d = i + 2
+        cov = stats.wishart(df=10 + d, scale=np.eye(d)).rvs(size=1)
+        mean = rng.standard_normal(size=d)
+        X = rng.standard_normal(size=(11, d))
+        den_estimate = density.GaussianMixture.multivariate_normal_density(
+            mean, cov, X
+        )
 
-            mnorm = stats.multivariate_normal(mean=mean, cov=cov)
-            den_truth = mnorm.pdf(X)
+        mnorm = stats.multivariate_normal(mean=mean, cov=cov)
+        den_truth = mnorm.pdf(X)
 
-            np.testing.assert_almost_equal(den_estimate, den_truth)
+        testing.assert_almost_equal(den_estimate, den_truth)
--- hyppo/kgof/tests/test_fssd.py
+++ hyppo/kgof/tests/test_fssd.py
@@ -2,82 +2,81 @@ import numpy as np
 import numpy.testing as testing
 import matplotlib.pyplot as plt
 
-from .. import fssd, data, density, _utils, kernel, h0simulator
+from .. import FSSD, data, density, _utils, kernel, h0simulator
 
-from fssd import FSSD
 from numpy.random import default_rng
 
 import scipy.stats as stats
 import unittest
+import pytest
 
 
 class TestFSSD(unittest.TestCase):
-
-    def test_basic(self):
+    @pytest.mark.parametrize("n", [100])
+    @pytest.mark.parametrize("alpha", [0.01])
+    @pytest.mark.parametrize("d", [1, 4])
+    @pytest.mark.parametrize("J", [1, 3])
+    def test_basic(self, n, alpha, d, J):
         seed = 12
         # sample
-        n = 100
-        alpha = 0.01
-        for d in [1, 4]:
-            mean = np.zeros(d)
-            variance = 1
-            isonorm = density.IsotropicNormal(mean, variance)
+        mean = np.zeros(d)
+        variance = 1
+        isonorm = density.IsotropicNormal(mean, variance)
 
-            # only one dimension of the mean is shifted
-            draw_mean = mean + 0
-            draw_variance = variance + 1
+        # only one dimension of the mean is shifted
+        draw_mean = mean + 0
+        draw_variance = variance + 1
 
-            rng = default_rng(seed)
-            X = rng.standard_normal(size=(n, d)) * np.sqrt(draw_variance) + draw_mean
-            dat = data.Data(X)
+        rng = default_rng(seed)
+        X = rng.standard_normal(size=(n, d)) * np.sqrt(draw_variance) + draw_mean
+        dat = data.Data(X)
 
-            # Test
-            for J in [1, 3]:
-                sig2 = _utils.meddistance(X, subsample=1000) ** 2
-                k = kernel.KGauss(sig2)
+        # Test
+        sig2 = _utils.meddistance(X, subsample=1000) ** 2
+        k = kernel.KGauss(sig2)
 
-                # random test locations
-                V = _utils.fit_gaussian_draw(X, J, seed=seed + 1)
-                null_sim = h0simulator.FSSDH0SimCovObs(n_simulate=200, seed=3)
-                fssd = FSSD(isonorm, k, V, null_sim=null_sim, alpha=alpha)
+        # random test locations
+        V = _utils.fit_gaussian_draw(X, J, seed=seed + 1)
+        null_sim = h0simulator.FSSDH0SimCovObs(n_simulate=200, seed=3)
+        fssd = FSSD(isonorm, k, V, null_sim=null_sim, alpha=alpha)
 
-                tresult = fssd.test(dat, return_simulated_stats=True)
+        tresult = fssd.test(dat, return_simulated_stats=True)
 
-                # assertions
-                self.assertGreaterEqual(tresult["pvalue"], 0)
-                self.assertLessEqual(tresult["pvalue"], 1)
+        # assertions
+        self.assertGreaterEqual(tresult["pvalue"], 0)
+        self.assertLessEqual(tresult["pvalue"], 1)
 
-    def test_ustat_h1_mean_variance(self):
+    @pytest.mark.parametrize("n", [200])
+    @pytest.mark.parametrize("alpha", [0.01])
+    @pytest.mark.parametrize("d", [1, 4])
+    @pytest.mark.parametrize("J", [1, 3])
+    def test_ustat_h1_mean_variance(self, n, alpha, d, J):
         seed = 20
         # sample
-        n = 200
-        alpha = 0.01
-        for d in [1, 4]:
-            mean = np.zeros(d)
-            variance = 1
-            isonorm = density.IsotropicNormal(mean, variance)
+        mean = np.zeros(d)
+        variance = 1
+        isonorm = density.IsotropicNormal(mean, variance)
 
-            draw_mean = mean + 2
-            draw_variance = variance + 1
-            rng = default_rng(seed)
-            X = rng.standard_normal(size=(n, d)) * np.sqrt(draw_variance) + draw_mean
-            dat = data.Data(X)
+        draw_mean = mean + 2
+        draw_variance = variance + 1
+        rng = default_rng(seed)
+        X = rng.standard_normal(size=(n, d)) * np.sqrt(draw_variance) + draw_mean
+        dat = data.Data(X)
 
-            # Test
-            for J in [1, 3]:
-                sig2 = _utils.meddistance(X, subsample=1000) ** 2
-                k = kernel.KGauss(sig2)
+        # Test
+        sig2 = _utils.meddistance(X, subsample=1000) ** 2
+        k = kernel.KGauss(sig2)
 
-                # random test locations
-                V = _utils.fit_gaussian_draw(X, J, seed=seed + 1)
+        # random test locations
+        V = _utils.fit_gaussian_draw(X, J, seed=seed + 1)
 
-                null_sim = h0simulator.FSSDH0SimCovObs(n_simulate=200, seed=3)
-                fssd = FSSD(isonorm, k, V, null_sim=null_sim, alpha=alpha)
-                fea_tensor = fssd.feature_tensor(X)
+        null_sim = h0simulator.FSSDH0SimCovObs(n_simulate=200, seed=3)
+        fssd = FSSD(isonorm, k, V, null_sim=null_sim, alpha=alpha)
+        fea_tensor = fssd.feature_tensor(X)
 
-                u_mean, u_variance = FSSD.ustat_h1_mean_variance(fea_tensor)
+        u_mean, u_variance = FSSD.ustat_h1_mean_variance(fea_tensor)
 
-                # assertions
-                self.assertGreaterEqual(u_variance, 0)
-                # should reject H0
-                self.assertGreaterEqual(u_mean, 0)
+        # assertions
+        self.assertGreaterEqual(u_variance, 0)
+        # should reject H0
+        self.assertGreaterEqual(u_mean, 0)
