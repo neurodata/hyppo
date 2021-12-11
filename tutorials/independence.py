1ae8af9c8640023e438ea304dce8df63bff7800e
dlee0156 <d0156lee@gmail.com>
2021-12-11 15:39:12 -0500

    Add tutorial for dHsic

--- tutorials/independence.py
+++ tutorials/independence.py
@@ -138,32 +138,6 @@ print(u"Fast time (fast statistic chi-square): {0:.3g}s".format(fast_chisq_time)
 # has a sample size greater than 20, then the test will run.
 # In the case where the data is 1 dimensional and Euclidean, an even faster version is
 # run.
-#
-# ------------
-#
-# **D-variable Hilbert Schmidt Independence Criterion (Dhsic)** is an extension of Hsic,
-# and it tests for the joint independence of d-random variables. More details can be
-# found in :class:`hyppo.multivariate.dhsic`. Note that unlike Hsic or Dcorr, there is no
-# fast version of the test. It always uses the permutation method to compute its p-value.
-#
-# .. note::
-#
-#    :Pros: - Accurate, powerful independence test for more than two random variables
-#           - Much faster than constructing a joint independence test from multiple
-#             pairwise independence tests
-#    :Cons: - Not guaranteed to be consistent
-
-setup_code = """
-from hyppo.independence import Dhsic
-from hyppo.tools import w_shaped
-x1, y1 = w_shaped(n=100, p=3, noise=True)
-x2, y2 = w_shaped(n=100, p=3, noise=True
-"""
-
-t_perm = timeit.Timer(stmt="Dhsic().test(*(x1, y1, x2, y2))", setup=setup_code)
-perm_time = np.array(t_perm.timeit(number=1))  # permutation Dhsic
-
-print(u"Permutation time: {0:.3g}s".format(perm_time))
 
 ########################################################################################
 # ------------
