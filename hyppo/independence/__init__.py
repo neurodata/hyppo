df088ca51368402c7271c10ef1a33b906f99eb7a
zdbzdb123123 <95720890+zdbzdb123123@users.noreply.github.com>
2021-12-13 09:07:54 -0500

    Update __init__.py

--- hyppo/independence/__init__.py
+++ hyppo/independence/__init__.py
@@ -7,6 +7,7 @@ from .kmerf import KMERF
 from .max_margin import MaxMargin
 from .mgc import MGC
 from .rv import RV
+from .friedmanRafsky import FriedmanRafsky
 
 __all__ = [s for s in dir()]  # add imported tests to __all__
 
@@ -20,4 +21,5 @@ INDEP_TESTS = {
     "mgc": MGC,
     "kmerf": KMERF,
     "maxmargin": MaxMargin,
+    "friedmanRafsky": FriedmanRafsky
 }
