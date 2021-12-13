58352a33da3759000dfe5d9bcf1ee1716a7a20e5
MatthewZhao26 <52184663+MatthewZhao26@users.noreply.github.com>
2021-12-13 00:05:40 -0500

    coverage

--- hyppo/ksample/tests/test_mean_embedding.py
+++ hyppo/ksample/tests/test_mean_embedding.py
@@ -4,6 +4,7 @@ from numpy.testing import assert_almost_equal
 
 from hyppo.tools import rot_ksamp
 from hyppo.ksample import MeanEmbeddingTest
+from hyppo.ksample.mean_embedding import _get_estimate
 
 
 class TestMeanEmbedding:
@@ -21,3 +22,16 @@ class TestMeanEmbedding:
         print(stat, pvalue)
         assert_almost_equal(stat, obs_stat, decimal=2)
         assert_almost_equal(pvalue, obs_pvalue, decimal=10)
+
+    @pytest.mark.parametrize(
+        "actual_norm",
+        [
+            ([0.00637311, 0.59369549, 0.16233412, 0.32396812, 0.05284812]),
+        ],
+    )
+    def test_norm(self, actual_norm):
+        np.random.seed(123456789)
+        x = np.random.randn(5, 2)
+        point = np.random.randn(5, 2)
+        norm = _get_estimate(x, point)
+        assert_almost_equal(norm, actual_norm)
