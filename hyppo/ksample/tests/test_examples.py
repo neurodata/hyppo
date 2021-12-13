e0659a4997449fa22b567d6db0c64ca1a46513cd
MatthewZhao26 <52184663+MatthewZhao26@users.noreply.github.com>
2021-12-12 22:49:06 -0500

    deleted

--- hyppo/ksample/tests/test_examples.py
+++
@@ -1,8 +0,0 @@
-import numpy as np
-from hyppo.ksample import SmoothCFTest
-
-np.random.seed(1234)
-x = np.random.randn(500, 10)
-y = np.random.randn(500, 10)
-stat, pvalue = SmoothCFTest(random_state=1234).test(x, y)
-print(stat, pvalue)
