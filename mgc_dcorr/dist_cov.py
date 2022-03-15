60e483f0d98f3db2aac35d615072f4bce93bca73
Dante Basile <dbasile1070@gmail.com>
2022-03-15 13:17:04 -0400

    updated args and nb, started dca algo

--- mgc_dcorr/dist_cov.py
+++ mgc_dcorr/dist_cov.py
@@ -1,11 +1,11 @@
 import numpy as np
 from numpy import linalg as LA
 
-def dist_mat(X, N):
+def dist_mat(X):
     """
-    First N samples of vector X to distance matrix D
+    Vector X to distance matrix D
     """
-    #N = len(X)
+    N = len(X)
     D = np.zeros((N, N))
     for i in range(N):
         for j in range(N):
@@ -16,20 +16,19 @@ def re_centered_dist(D):
     """
     Distance matrix D to re-centered distance matrix R
     """
-    N = D.shape[0] # D should be square NxN
+    N = D.shape[0] # D should be square NxN, where N is len(X)
     R = np.zeros_like(D)
     for i in range(N):
         for j in range(N):
             R[i, j] = D[i, j] - np.mean(D[:, j]) - np.mean(D[i, :]) + np.mean(D)
     return R
 
-def re_centered_dist_u(u, X, N):
-    return  re_centered_dist(dist_mat(X @ u, N))
+def re_centered_dist_u(u, X):
+    return  re_centered_dist(dist_mat(X @ u))
 
 def dist_cov_sq(R_X, R_Y):
     """
     Are X and Y same length?
-    What is N? N is some subset < len(X) and len(Y)?
     """
     N = R_X.shape[0] # if R square and same length
     v_sum = 0.
@@ -38,10 +37,9 @@ def dist_cov_sq(R_X, R_Y):
             v_sum += R_X[i, j] * R_Y[i, j]
     return (1 / N**2) * v_sum
 
-def dist_cov_sq_grad(u, X, Y, R_X, R_Y):
+def dist_cov_sq_grad(u, X, R_Y):
     """
     Gradient for use in projected stochastic gradient descent optimization
-    Some args not needed?
     """
     def delta(u, i, j):
         sign_term = np.squeeze(np.sign((X[i] - X[j]) @ u))
@@ -70,13 +68,55 @@ def clamp_u(u):
     else:
         return u
 
-def optim_u_gd(u, X, Y, R_X, R_Y, lr, num_iter):
+def optim_u_gd(u, X, R_Y, lr, epsilon):
     """
     Gradient ascent for v^2 with respect to u
     """
     u_opt = np.copy(u)
-    for _ in range(num_iter):
-        grad = dist_cov_sq_grad(u_opt, X, Y, R_X, R_Y)
+    R_X_u = re_centered_dist_u(u, X)
+    v = dist_cov_sq(R_Y, R_X_u)
+    while True:
+        grad = dist_cov_sq_grad(u_opt, X, R_Y)
         u_opt += lr * grad # "+=": gradient ascent
         u_opt = clamp_u(u_opt)
+        R_X_u_opt = re_centered_dist_u(u_opt, X)
+        v_opt = dist_cov_sq(R_Y, R_X_u_opt)
+        delta = LA.norm(v_opt- v)
+        if delta <= epsilon:
+            break
+        else:
+            v = v_opt
     return u_opt
+
+def k_test():
+    """
+    Test if U[:, k] is significant with respect to U[:, 1:k-1]
+    Permutation test not needed for single dataset X
+    """
+
+def proj_U(X, U, k):
+    """
+    Project X onto the orthogonal subspace of k dim of U
+    """
+
+def dca(X, Y, lr, epsilon):
+    """
+    Perform DCA dimensionality reduction on X
+    Single dataset X
+    """
+    k = 0
+    delta = epsilon + 1
+    U = np.zeros_like(X.T) # kmax is num of X features?
+    X_proj = np.copy(X)
+    R_Y = re_centered_dist(Y)
+    while True:
+        U[:, k] = clamp_u(np.random.rand(X.shape[1], 1))
+        R_X_proj = re_centered_dist(X_proj)
+        u_opt = optim_u_gd(U[:, k], X_proj, Y, R_X_proj, R_Y, lr, epsilon)
+        U[:, k] = u_opt
+        if k_test(U, k):
+            X_proj = proj_U(X_proj, U[:, 0:k])
+            k += 1
+        else:
+            break
+    return X_proj
