490409510895571f3edb343e3ce3572d32d61b44
zdbzdb123123 <95720890+zdbzdb123123@users.noreply.github.com>
2022-03-17 11:32:30 -0400

    Update kci.py

--- hyppo/independence/kci.py
+++ hyppo/independence/kci.py
@@ -11,7 +11,16 @@ class KCI(IndependenceTest):
 
         IndependenceTest.__init__(self, **kwargs)
 
-    def test(self, x, y):
+    def kernel(self, x, y):
+        theta = 1 / (width**2)
+
+        Kx = 1.0 * RBF(theta).diag(x)
+        Ky = 1.0 * RBF(theta).diag(y)
+
+        return Kx, Ky
+
+
+    def statistic(self, x, y):
         T = len(y)
 
         x = np.array(x)
@@ -28,23 +37,28 @@ class KCI(IndependenceTest):
         else:
             width = 0.3
 
-        theta = 1 / (width**2)
 
         H = np.eye(T) - np.ones((T, T)) / T
 
-        Kx = 1.0 * RBF(theta).diag(x)
-        Ky = 1.0 * RBF(theta).diag(y)
+        Kx, Ky = self.kernel(x, y)
 
         Kx = np.matmul(np.matmul(H, Kx), H)
-
         Ky = np.matmul(np.matmul(H, Ky), H)
-        Stat = np.matmul(Kx * Ky)
+
+        stat = np.matmul(Kx * Ky)
+
+        return stat
+
+    def test(x, y):
+
+        Kx, Ky = self.kernel(x, y)
+        stat = self.statistic(x, y)
 
         mean_appr = (Kx * Ky) / T
         var_appr = 2 * (Kx * Kx) * (Ky * Ky) / T**2
         k_appr = mean_appr**2 / var_appr
         theta_appr = var_appr / mean_appr
-        pvalue = 1 - np.mean(gamma.cdf(Stat, k_appr, theta_appr))
+        pvalue = 1 - np.mean(gamma.cdf(stat, k_appr, theta_appr))
 
         self.stat = Stat
 
