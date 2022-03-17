b9e70e75c1050f356cb278bd50e304d61e421dbf
zdbzdb123123 <95720890+zdbzdb123123@users.noreply.github.com>
2022-03-17 12:17:12 -0400

    Update kci.py

--- hyppo/independence/kci.py
+++ hyppo/independence/kci.py
@@ -31,8 +31,8 @@ class KCI(IndependenceTest):
 
         theta = 1 / (width**2)
 
-        Kx = 1.0 * RBF(theta).diag(x)
-        Ky = 1.0 * RBF(theta).diag(y)
+        Kx = 1.0 * RBF(theta).__call__(x, x)
+        Ky = 1.0 * RBF(theta).__call__(y, y)
 
         return Kx, Ky
 
@@ -47,7 +47,7 @@ class KCI(IndependenceTest):
         Kx = np.matmul(np.matmul(H, Kx), H)
         Ky = np.matmul(np.matmul(H, Ky), H)
 
-        stat = np.matmul(Kx, Ky)
+        stat = np.trace(np.matmul(Kx, Ky))
 
         return stat
 
@@ -58,8 +58,8 @@ class KCI(IndependenceTest):
         Kx, Ky = self.kernel(x, y)
         stat = self.statistic(x, y)
 
-        mean_appr = (Kx * Ky) / T
-        var_appr = 2 * (Kx * Kx) * (Ky * Ky) / T**2
+        mean_appr = (np.trace(Kx) * np.trace(Ky)) / T
+        var_appr = 2 * np.trace(np.matmul(Kx, Kx)) * np.trace(np.matmul(Ky, Ky)) / T**2
         k_appr = mean_appr**2 / var_appr
         theta_appr = var_appr / mean_appr
         pvalue = 1 - np.mean(gamma.cdf(stat, k_appr, theta_appr))
