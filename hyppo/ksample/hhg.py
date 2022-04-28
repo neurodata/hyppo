9fd4cb5a46e061fc0216b51b983510f6952ac981
TacticalFallacy <limjianloong@gmail.com>
2022-04-29 01:25:12 +0900

    Remove Center of Mass Version

--- hyppo/ksample/hhg.py
+++ hyppo/ksample/hhg.py
@@ -7,28 +7,21 @@ from scipy.stats import ks_2samp
 
 class HHG(KSampleTest):
     r"""
-    Fast HHG 2-Sample Test
+    Fast HHG 2-Sample Test: 
     """
-    def __init__(self, compute_distance="euclidean", mode ="CM", **kwargs):
+    def __init__(self, compute_distance="euclidean", **kwargs):
        self.compute_distance = compute_distance
-       self.mode = mode
        KSampleTest.__init__(self, compute_distance=compute_distance, **kwargs)
        
     def statistic(self, y1, y2):
-        if self.mode == "CM":
-            disty1, disty2 = _centerpoint_dist_CM(y1, y2, self.compute_distance, 1)
-            stat, pvalue = ks_2samp(disty1, disty2)
-            self.stat = stat
-            return stat
-        if self.mode == "MP":
-            y = np.concatenate((y1,y2), axis=0)
-            disty = _centerpoint_dist_MP(y, self.compute_distance, 1)
-            disty1 = disty[:,0:len(y1)]
-            disty2 = disty[:,len(y1):len(y2)+len(y1)]
-            pvalues = _distance_score_MP(disty1,disty2)
-            stat = min(pvalues)*len(pvalues)
-            self.stat = stat
-            return stat
+        y = np.concatenate((y1,y2), axis=0)
+        disty = _centerpoint_dist(y, self.compute_distance, 1)
+        disty1 = disty[:,0:len(y1)]
+        disty2 = disty[:,len(y1):len(y2)+len(y1)]
+        pvalues = _distance_score(disty1,disty2)
+        stat = min(pvalues)*len(pvalues)
+        self.stat = stat
+        return stat
 
     def test(self, x, y):
         """
@@ -41,43 +34,22 @@ class HHG(KSampleTest):
         check_input = _CheckInputs(
             inputs=[x, y],
         )
-        if self.mode == "CM":
-            distx, disty = _centerpoint_dist_CM(x, y, self.compute_distance, 1)
-            stat, pvalue = ks_2samp(distx,disty)
-            return stat, pvalue
-        elif self.mode == "MP":
-            xy = np.concatenate((x,y), axis=0)
-            distxy = _centerpoint_dist_MP(xy, self.compute_distance, 1)
-            distx = distxy[:,0:len(x)]
-            disty = distxy[:,len(x):len(x)+len(x)]
-            pvalues = _distance_score_MP(distx,disty)
-            stat = min(pvalues)*len(pvalues)
-            return stat
+        x, y = check_input()
         
-def _centerpoint_dist_CM(x, y, metric, workers=1, **kwargs):
-    #CM of one group as center point
-    zx = np.mean(x, axis=0)
-    zx = np.array(zx).reshape(1,-1)
-    xin = np.concatenate((zx, x))
-    yin = np.concatenate((zx, y))
-    if callable(metric):
-        disty1 = metric(xin, **kwargs)
-        disty1 = metric(yin, **kwargs)
-    else:
-        disty1 = pairwise_distances(xin, metric=metric, n_jobs=workers, **kwargs)
-        disty2 = pairwise_distances(yin, metric=metric, n_jobs=workers, **kwargs)
-    disty1 = np.delete(disty1[0],0)
-    disty2 = np.delete(disty2[0],0)
-    disty1 = disty1.reshape(-1)
-    disty2 = disty2.reshape(-1)
-    return disty1, disty2
+        xy = np.concatenate((x,y), axis=0)
+        distxy = _centerpoint_dist(xy, self.compute_distance, 1)
+        distx = distxy[:,0:len(x)]
+        disty = distxy[:,len(x):len(x)+len(x)]
+        pvalues = _distance_score(distx,disty)
+        stat = min(pvalues)*len(pvalues)
+        return stat
     
-def _centerpoint_dist_MP(xy, metric, workers=1, **kwargs):
+def _centerpoint_dist(xy, metric, workers=1, **kwargs):
     disty = pairwise_distances(xy, metric=metric, n_jobs=workers, **kwargs)
     return disty
 
-def _distance_score_MP(distx, disty):
-    dist1, dist2 = _extract_distance_MP(distx, disty)
+def _distance_score(distx, disty):
+    dist1, dist2 = _extract_distance(distx, disty)
     pvalues = []
     for i in range(len(distx)):
         stat, pvalue = ks_2samp(dist1[i],dist2[i])
@@ -85,7 +57,7 @@ def _distance_score_MP(distx, disty):
     return pvalues
 
 @jit(nopython=True)
-def _extract_distance_MP(disty1, disty2):
+def _extract_distance(disty1, disty2):
     dist1 = []
     dist2 = []
     for i in range(len(disty1)):
--- hyppo/ksample/tests/test_hhg.py
+++ hyppo/ksample/tests/test_hhg.py
@@ -7,20 +7,17 @@ from .. import HHG
 
 class TestHHG:
     @pytest.mark.parametrize(
-        "n, obs_stat_CM, obs_pvalue_CM, obs_stat_MP",
+        "n, obs_stat_MP",
         [
-            (100, 0.04, 0.9999988, 4.912e-10),
-            (10, 0.3, 0.7869297, 0.125874),
+            (100, 4.912e-10),
+            (10, 0.125874),
         ],
     )
-    def test_linear_oned(self, n, obs_stat_CM, obs_pvalue_CM, obs_stat_MP):
+    def test_linear_oned(self, n, obs_stat_MP):
         np.random.seed(123456789)
         x, y = rot_ksamp("linear", n, 1, k=2, noise=False)
-        CMstat, pvalue = HHG(mode ="CM").test(x, y)
-        MPstat = HHG(mode ="MP").test(x, y)
+        MPstat = HHG().test(x, y)
         
-        assert_almost_equal(CMstat, obs_stat_CM, decimal=2)
-        assert_almost_equal(pvalue, obs_pvalue_CM, decimal=2)
         assert_almost_equal(MPstat, obs_stat_MP, decimal=2)
         
     @pytest.mark.parametrize(
@@ -30,26 +27,25 @@ class TestHHG:
     def test_rep(self, n, obs_stat, obs_pvalue):
         np.random.seed(123456789)
         x, y = rot_ksamp("linear", n, 1, k=2, noise=False)
-        CMstat1, pvalue1 = HHG(mode ="CM").test(x, y)
-        CMstat2, pvalue2 = HHG(mode ="CM").test(x, y)
         MPstat1 = HHG(mode ="MP").test(x, y)
         MPstat2 = HHG(mode ="MP").test(x, y)
         
-        assert CMstat1 == CMstat2
-        assert pvalue1 == pvalue2
         assert MPstat1 == MPstat2
     
 class TestHHGTypeIError:
     def test_oned(self):
         np.random.seed(123456789)
-        est_power = power(
-            "HHG",
-            sim_type="ksamp",
-            sim="multimodal_independence",
-            k=2,
-            n=100,
-            p=1,
-            alpha=0.05,
-        )
+        rejections=0
+        for i in range(1000):
+            x, y = rot_ksamp(
+                "multimodal_independence",
+                n=100,
+                p=1,
+                noise=True,
+                degree=90)
+            MPstat = HHG().test(x,y)
+        if MPstat < 0.05:
+            rejections += 1
+        est_power = rejections/1000
 
-        assert_almost_equal(est_power, 0.05, decimal=2)
+        assert_almost_equal(est_power, 0, decimal=3)
