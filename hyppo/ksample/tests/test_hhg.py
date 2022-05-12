4294a991d2448a5e0240f40aab097b9cd5cee299
TacticalFallacy <limjianloong@gmail.com>
2022-05-13 03:14:24 +0900

    Update unit tests

--- hyppo/ksample/tests/test_hhg.py
+++ hyppo/ksample/tests/test_hhg.py
@@ -2,29 +2,30 @@ import numpy as np
 import pytest
 from numpy.testing import assert_almost_equal
 
-from ...tools import power, rot_ksamp
+from ...tools import rot_ksamp
 from .. import HHG
 
 
 class TestHHG:
     @pytest.mark.parametrize(
-        "n, obs_stat_MP", [(100, 4.912e-10), (10, 0.125874),],
+        "n, obs_stat, obs_pvalue", [(100, 0.515, 4.912e-10), (10, 0.777, 0.125874),],
     )
-    def test_linear_oned(self, n, obs_stat_MP):
+    def test_linear_oned(self, n, obs_stat, obs_pvalue):
         np.random.seed(123456789)
         x, y = rot_ksamp("linear", n, 1, k=2, noise=False)
-        MPstat = HHG().test(x, y)
+        stat, pvalue = HHG().test(x, y)
 
-        assert_almost_equal(MPstat, obs_stat_MP, decimal=2)
+        assert_almost_equal(stat, obs_stat, decimal=3)
+        assert_almost_equal(pvalue, obs_pvalue, decimal=3)
 
     @pytest.mark.parametrize(
-        "n, obs_stat, obs_pvalue", [(100, 8.24e-5, 0.001)],
+        "n", [(100)],
     )
-    def test_rep(self, n, obs_stat, obs_pvalue):
+    def test_rep(self, n):
         np.random.seed(123456789)
         x, y = rot_ksamp("linear", n, 1, k=2, noise=False)
-        MPstat1 = HHG(mode="MP").test(x, y)
-        MPstat2 = HHG(mode="MP").test(x, y)
+        MPstat1 = HHG().test(x, y)
+        MPstat2 = HHG().test(x, y)
 
         assert MPstat1 == MPstat2
 
@@ -37,9 +38,9 @@ class TestHHGTypeIError:
             x, y = rot_ksamp(
                 "multimodal_independence", n=100, p=1, noise=True, degree=90
             )
-            MPstat = HHG().test(x, y)
-        if MPstat < 0.05:
+            stat, pvalue = HHG().test(x, y)
+        if pvalue < 0.05:
             rejections += 1
         est_power = rejections / 1000
 
-        assert_almost_equal(est_power, 0, decimal=3)
+        assert_almost_equal(est_power, 0, decimal=2)
