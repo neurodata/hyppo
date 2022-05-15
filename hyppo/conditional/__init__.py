bf71afa6a188f6aa658426f7b1b4aa0bdea12afc
zdbzdb123123 <95720890+zdbzdb123123@users.noreply.github.com>
2022-05-15 16:20:16 -0400

    Update __init__.py

--- hyppo/conditional/__init__.py
+++ hyppo/conditional/__init__.py
@@ -1,11 +1,6 @@
-from .kci import KCI
 from .FCIT import FCIT
-
+from .kci import KCI
 
 __all__ = [s for s in dir()]  # add imported tests to __all__
 
-
-COND_INDEP_TESTS = {
-    "kci": KCI,
-    "fcit":FCIT
-}
+COND_INDEP_TESTS = {"fcit":FCIT, "kci": KCI,}
