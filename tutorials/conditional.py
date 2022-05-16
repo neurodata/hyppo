7803ccdd5c8f186ccbe315c835628de71bc41532
zdbzdb123123 <95720890+zdbzdb123123@users.noreply.github.com>
2022-05-16 12:25:50 -0400

    Update conditional.py

--- tutorials/conditional.py
+++ tutorials/conditional.py
@@ -83,15 +83,13 @@ print("p-value: ", pvalue)
 #
 #    :Pros: - Very fast on high-dimensional data due to simplicity and approximation
 #    :Cons: - Dispute in literature as to ideal theta value, loss of accuracy on very large datasets
+#
 # Below is a linear example where we fail to reject the null hypothesis:
 
-setup_code = """
 from hyppo.conditional import KCI
 from hyppo.tools import linear
 np.random.seed(123456789)
 x, y = linear(n, 1)
-"""
-
 stat, pvalue = KCI().test(x, y)
 print("Statistic: ", stat)
 print("p-value: ", pvalue)
