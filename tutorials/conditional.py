f9e6fa8c16b173663286d7d0993b493938164d0e
zdbzdb123123 <95720890+zdbzdb123123@users.noreply.github.com>
2022-05-16 11:50:19 -0400

    Update conditional.py

--- tutorials/conditional.py
+++ tutorials/conditional.py
@@ -86,7 +86,7 @@ print("p-value: ", pvalue)
 # Below is a linear example where we fail to reject the null hypothesis:
 
 from hyppo.conditional import KCI
-from hyppo.tools.indep_sim import linear
+from hyppo.tools import linear
 np.random.seed(123456789)
 x, y = linear(n, 1)
 stat, pvalue = KCI().test(x, y)
