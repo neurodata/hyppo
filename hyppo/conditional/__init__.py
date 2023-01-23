246735a035b97a9e9df808f15df1e04dbcdbf15d
j1c <jaewonc78@gmail.com>
2023-01-23 15:29:08 -0500

    Separate cdcorr and cdcov

--- hyppo/conditional/__init__.py
+++ hyppo/conditional/__init__.py
@@ -1,8 +1,7 @@
+from .cdcorr import CDcorr, CDcov
 from .FCIT import FCIT
 from .kci import KCI
-from .cdcorr import CDcorr
-
 
 __all__ = [s for s in dir()]  # add imported tests to __all__
 
-COND_INDEP_TESTS = {"fcit": FCIT, "kci": KCI, "cdcorr": CDcorr}
+COND_INDEP_TESTS = {"fcit": FCIT, "kci": KCI, "cdcorr": CDcorr, "cdcov": CDcov}
--- hyppo/conditional/cdcorr.py
+++ hyppo/conditional/cdcorr.py
@@ -5,14 +5,12 @@ from scipy.spatial.distance import pdist, squareform
 
 from ..tools import compute_dist, perm_test
 from ._utils import _CheckInputs
-
-# from ._utils import _CheckInputs
 from .base import ConditionalIndependenceTest, ConditionalIndependenceTestOutput
 
 
-class CDcorr(ConditionalIndependenceTest):
+class CDcov(ConditionalIndependenceTest):
     r"""
-    Conditional Distance Correlation (CDcorr) test statistic and p-value.
+    Conditional Distance Covariance (CDcorr) test statistic and p-value.
 
 
     Reference
@@ -34,34 +32,6 @@ class CDcorr(ConditionalIndependenceTest):
 
         ConditionalIndependenceTest.__init__(self, **kwargs)
 
-    def _compute_kern(self, data):
-        n, d = data.shape
-
-        if self.bandwidth is None:
-            # Assumes independent variables
-            # Scott's rule of thumb
-            factor = np.power(n, (-1.0 / (d + 4)))
-            stds = np.std(data, axis=0, ddof=1)
-            bandwidth_ = factor * stds
-
-            # TODO Implement silverman's rule of thumb
-        elif isinstance(self.bandwidth, (int, float)):
-            bandwidth_ = np.repeat(self.bandwidth, d)
-        else:
-            bandwidth_ = self.bandwidth
-
-        # Compute constants
-        denom = np.power(2 * np.pi, d / 2) * np.power(bandwidth_.prod(), 0.5)
-
-        sim_mat = pdist(
-            data, "sqeuclidean", w=1 / bandwidth_**2
-        )  # Section 4.2 equation
-        sim_mat = squareform(-0.5 * sim_mat)
-        np.exp(sim_mat, sim_mat)
-        sim_mat /= denom
-
-        return sim_mat
-
     def statistic(self, x, y, z):
         distx = x
         disty = y
@@ -73,7 +43,7 @@ class CDcorr(ConditionalIndependenceTest):
                 y,
                 metric=self.compute_distance,
             )
-            distz = self._compute_kern(z)
+            distz, bandwidth_ = _compute_kern(z, self.bandwidth)
 
         stat = _cdcov(distx, disty, distz).mean()
         self.stat = stat
@@ -120,6 +90,89 @@ class CDcorr(ConditionalIndependenceTest):
 
         return ConditionalIndependenceTestOutput(stat, pvalue)
 
+
+class CDcorr(ConditionalIndependenceTest):
+    r"""
+    Conditional Distance Correlation (CDcorr) test statistic and p-value.
+
+
+    Reference
+    ---------
+    D.W. Scott, “Multivariate Density Estimation: Theory, Practice, and Visualization”, John Wiley & Sons, New York, Chicester, 1992.
+    """
+
+    def __init__(self, compute_distance="euclidean", bandwidth=None, **kwargs):
+        r"""
+        bandwith :
+        """
+        self.compute_distance = compute_distance
+        self.bandwidth = bandwidth
+
+        # set is_distance to true if compute_distance is None
+        self.is_distance = False
+        if not compute_distance:
+            self.is_distance = True
+
+        ConditionalIndependenceTest.__init__(self, **kwargs)
+
+    def statistic(self, x, y, z):
+        distx = x
+        disty = y
+        distz = z
+
+        if not self.is_distance:
+            distx, disty = compute_dist(
+                x,
+                y,
+                metric=self.compute_distance,
+            )
+            distz, bandwidth_ = _compute_kern(z, self.bandwidth)
+
+        stat = _cdcorr(distx, disty, distz).mean()
+        self.stat = stat
+
+        return stat
+
+    def test(
+        self,
+        x,
+        y,
+        z,
+        reps=1000,
+        workers=1,
+        random_state=None,
+    ):
+        check_input = _CheckInputs(
+            x,
+            y,
+            z,
+            reps=reps,
+        )
+        x, y, z = check_input()
+
+        if not self.is_distance:
+            x, y = compute_dist(x, y, metric=self.compute_distance, **self.kwargs)
+            z = self._compute_kern(z)
+
+            self.is_distance = True
+
+        stat, pvalue, null_dist = perm_test(
+            self.statistic,
+            x,
+            y,
+            z,
+            reps=reps,
+            workers=workers,
+            is_distsim=self.is_distance,
+            random_state=random_state,
+            permuter=partial(_permuter, probs=z),
+        )
+        self.stat = stat
+        self.pvalue = pvalue
+        self.null_dist = null_dist
+
+        return ConditionalIndependenceTestOutput(stat, pvalue)
+
     def _permuter(self, probs, rng, axis=1):
         """
         Weighted sampling with replacement
@@ -129,6 +182,44 @@ class CDcorr(ConditionalIndependenceTest):
         idx = ((probs / sums).cumsum(axis=1) > rng.rand(n)[:, None]).argmax(axis=1)
 
         return idx
+
+
+def _compute_kern(data, bandwidth):
+    n, d = data.shape
+
+    if bandwidth is None:
+        # Assumes independent variables
+        # Scott's rule of thumb
+        factor = np.power(n, (-1.0 / (d + 4)))
+        stds = np.std(data, axis=0, ddof=1)
+        bandwidth_ = factor * stds
+
+        # TODO Implement silverman's rule of thumb
+    elif isinstance(bandwidth, (int, float)):
+        bandwidth_ = np.repeat(bandwidth, d)
+    else:
+        bandwidth_ = bandwidth
+
+    # Compute constants
+    denom = np.power(2 * np.pi, d / 2) * np.power(bandwidth_.prod(), 0.5)
+
+    sim_mat = pdist(data, "sqeuclidean", w=1 / bandwidth_**2)  # Section 4.2 equation
+    sim_mat = squareform(-0.5 * sim_mat)
+    np.exp(sim_mat, sim_mat)
+    sim_mat /= denom
+
+    return sim_mat, bandwidth
+
+
+def _permuter(probs, rng, axis=1):
+    """
+    Weighted sampling with replacement
+    """
+    n = probs.shape[1 - axis]
+    sums = probs.sum(axis=1, keepdims=True)
+    idx = ((probs / sums).cumsum(axis=1) > rng.rand(n)[:, None]).argmax(axis=1)
+
+    return idx
 
 
 def _weighted_center_distmat(distx, weights):
