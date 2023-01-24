8e34be2bbe105033e496d07bdaa1c5a219dc0f56
j1c <jaewonc78@gmail.com>
2023-01-24 13:52:52 -0500

    Add docstrings

--- hyppo/conditional/cdcorr.py
+++ hyppo/conditional/cdcorr.py
@@ -12,21 +12,66 @@ class CDcorr(ConditionalIndependenceTest):
     r"""
     Conditional Distance Correlation (CDcorr) test statistic and p-value.
 
+    CDcorr is a measure of dependence between two paired random matrices
+    given a third random matrix of not necessarily equal dimensions :footcite:p:`wang2015conditional`.
+    The coefficient is 0 if and only if the matrices are independent given
+    third matrix.
 
-    Reference
-    ---------
-    D.W. Scott, “Multivariate Density Estimation: Theory, Practice, and Visualization”, John Wiley & Sons, New York, Chicester, 1992.
+    Parameters
+    ----------
+    compute_distance : str, callable, or None, default: "euclidean"
+        A function that computes the distance among the samples within each
+        data matrix.
+        Valid strings for ``compute_distance`` are, as defined in
+        :func:`sklearn.metrics.pairwise_distances`,
+
+            - From scikit-learn: [``"euclidean"``, ``"cityblock"``, ``"cosine"``,
+              ``"l1"``, ``"l2"``, ``"manhattan"``] See the documentation for
+              :mod:`scipy.spatial.distance` for details
+              on these metrics.
+            - From scipy.spatial.distance: [``"braycurtis"``, ``"canberra"``,
+              ``"chebyshev"``, ``"correlation"``, ``"dice"``, ``"hamming"``,
+              ``"jaccard"``, ``"kulsinski"``, ``"mahalanobis"``, ``"minkowski"``,
+              ``"rogerstanimoto"``, ``"russellrao"``, ``"seuclidean"``,
+              ``"sokalmichener"``, ``"sokalsneath"``, ``"sqeuclidean"``,
+              ``"yule"``] See the documentation for :mod:`scipy.spatial.distance` for
+              details on these metrics.
+
+        Set to ``None`` or ``"precomputed"`` if ``x`` and ``y`` are already distance
+        matrices. To call a custom function, either create the distance matrix
+        before-hand or create a function of the form ``metric(x, **kwargs)``
+        where ``x`` is the data matrix for which pairwise distances are
+        calculated and ``**kwargs`` are extra arguements to send to your custom
+        function.
+    use_cov : bool,
+        If `True`, then the statistic will compute the covariance rather than the
+        correlation.
+    bandwith : str, scalar, 1d-array
+        The method used to calculate the bandwidth used for kernel density estimate of
+        the conditional matrix. This can be ‘scott’, ‘silverman’, a scalar constant or a
+        1d-array with length ``r`` which is the dimensions of the conditional matrix.
+        If None (default), ‘scott’ is used.
+    **kwargs
+        Arbitrary keyword arguments for ``compute_distance``.
+
+    References
+    ----------
+    .. footbibliography::
     """
 
     def __init__(
-        self, use_cov=True, compute_distance="euclidean", bandwidth=None, **kwargs
+        self, compute_distance="euclidean", use_cov=True, bandwidth=None, **kwargs
     ):
-        r"""
-        bandwith :
-        """
         self.use_cov = use_cov
         self.compute_distance = compute_distance
         self.bandwidth = bandwidth
+
+        # Check bandwidth input
+        if bandwidth is not None:
+            if isinstance(bandwidth, (int, float, np.ndarray, str)):
+                raise ValueError(
+                    "`bandwidth` should be 'scott', 'silverman', a scalar or 1d-array."
+                )
 
         # set is_distance to true if compute_distance is None
         self.is_distance = False
@@ -36,6 +81,24 @@ class CDcorr(ConditionalIndependenceTest):
         ConditionalIndependenceTest.__init__(self, **kwargs)
 
     def statistic(self, x, y, z):
+        r"""
+        Helper function that calculates the Dcorr test statistic.
+
+        Parameters
+        ----------
+        x,y,z : ndarray of float
+            Input data matrices. ``x``, ``y`` and ``z`` must have the same number
+            of samples. That is, the shapes must be ``(n, p)``, ``(n, q)`` and
+            ``(n, r)`` where `n` is the number of samples and `p`, `q`, and `r`
+            are the number of dimensions. Alternatively, ``x`` and ``y`` can be
+            distance matrices and ``z`` can be a similarity matrix where the
+            shapes must be ``(n, n)``.
+
+        Returns
+        -------
+        stat : float
+            The computed Dcorr statistic.
+        """
         distx = x
         disty = y
         distz = z
@@ -65,6 +128,35 @@ class CDcorr(ConditionalIndependenceTest):
         workers=1,
         random_state=None,
     ):
+        r"""
+        Calculates the Dcorr test statistic and p-value.
+
+        Parameters
+        ----------
+        x,y,z : ndarray of float
+            Input data matrices. ``x``, ``y`` and ``z`` must have the same number
+            of samples. That is, the shapes must be ``(n, p)``, ``(n, q)`` and
+            ``(n, r)`` where `n` is the number of samples and `p`, `q`, and `r`
+            are the number of dimensions. Alternatively, ``x`` and ``y`` can be
+            distance matrices and ``z`` can be a similarity matrix where the
+            shapes must be ``(n, n)``.
+        reps : int, default: 1000
+            The number of replications used to estimate the null distribution
+            when using the permutation test used to calculate the p-value.
+        workers : int, default: 1
+            The number of cores to parallelize the p-value computation over.
+            Supply ``-1`` to use all cores available to the Process.
+        random_state : int, default: None
+            The random_state for permutation testing to be fixed for
+            reproducibility.
+
+        Returns
+        -------
+        stat : float
+            The computed CDcorr statistic.
+        pvalue : float
+            The computed CDcorr p-value.
+        """
         check_input = _CheckInputs(
             x,
             y,
@@ -107,6 +199,12 @@ class CDcorr(ConditionalIndependenceTest):
             bandwidth_ = factor * stds
 
             # TODO Implement silverman's rule of thumb
+
+            if "silverman":
+                factor = (n * (d + 2) / 4.0) ** (-1.0 / (d + 4))
+                stds = np.std(data, axis=0, ddof=1)
+                bandwidth_ = factor * stds
+
         elif isinstance(self.bandwidth, (int, float)):
             bandwidth_ = np.repeat(self.bandwidth, d)
         else:
@@ -115,6 +213,7 @@ class CDcorr(ConditionalIndependenceTest):
         # Compute constants
         denom = np.power(2 * np.pi, d / 2) * np.power(bandwidth_.prod(), 0.5)
 
+        # Compute weighted kernel
         sim_mat = pdist(
             data, "sqeuclidean", w=1 / bandwidth_**2
         )  # Section 4.2 equation
@@ -123,16 +222,6 @@ class CDcorr(ConditionalIndependenceTest):
         sim_mat /= denom
 
         return sim_mat
-
-    def _permuter(self, probs, rng, axis=1):
-        """
-        Weighted sampling with replacement
-        """
-        n = probs.shape[1 - axis]
-        sums = probs.sum(axis=1, keepdims=True)
-        idx = ((probs / sums).cumsum(axis=1) > rng.rand(n)[:, None]).argmax(axis=1)
-
-        return idx
 
 
 def _permuter(probs, rng, axis=1):
@@ -147,7 +236,7 @@ def _permuter(probs, rng, axis=1):
 
 
 def _weighted_center_distmat(distx, weights):
-    """Centers the distance matrices"""
+    """Centers the distance matrices with weights"""
 
     n = distx.shape[0]
 
@@ -161,6 +250,7 @@ def _weighted_center_distmat(distx, weights):
 
 
 def _cdcov(distx, disty, distz):
+    """Calculate the CDcov test statistic"""
     n = distx.shape[0]
 
     cdcov = np.zeros(n)
@@ -177,6 +267,7 @@ def _cdcov(distx, disty, distz):
 
 
 def _cdcorr(distx, disty, distz):
+    """Calculate the CDcorr test statistic"""
     varx = _cdcov(distx, distx, distz)
     vary = _cdcov(disty, disty, distz)
     covar = _cdcov(distx, disty, distz)
