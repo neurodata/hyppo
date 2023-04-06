23d2f4f3860fbe0983898b1b1ab3fc83e8c20ee5
Rudra Keshwani <rudrakeshwani2@gmail.com>
2023-04-06 13:04:10 +1000

    Update __init__.py
    Can't this all be simplified to a singular import statement

--- hyppo/__init__.py
+++ hyppo/__init__.py
@@ -1,10 +1,3 @@
-import hyppo.discrim
-import hyppo.independence
-import hyppo.ksample
-import hyppo.time_series
-import hyppo.kgof
-import hyppo.tools
-import hyppo.d_variate
-import hyppo.conditional
+import hyppo
 
 __version__ = "0.3.2"
