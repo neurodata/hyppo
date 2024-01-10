957b0d68f53caf069d206f589711bd3668169969
j1c <jaewonc78@gmail.com>
2024-01-10 16:23:24 -0500

    Add more tests

--- hyppo/conditional/tests/test_cdcorr.py
+++ hyppo/conditional/tests/test_cdcorr.py
@@ -1,24 +1,25 @@
 import numpy as np
 import pytest
-from numpy.testing import assert_almost_equal
+from numpy.testing import assert_almost_equal, assert_raises
 
 from ...tools import indep_normal, power
 from .. import ConditionalDcorr
 
 
-class TestCDcorrStat:
-    @pytest.mark.parametrize("n", [100, 200])
+class TestCDcorrStatPvalue:
+    @pytest.mark.parametrize("n", [50])
     @pytest.mark.parametrize("obs_stat", [0.0])
-    def test_linear_oned(self, n, obs_stat):
+    @pytest.mark.parametrize("bandwidth", [None, "silverman", np.ones(1)])
+    def test_indep_normal(self, n, obs_stat, bandwidth):
         np.random.seed(123456789)
         x, y, z = indep_normal(n, 1)
-        stat1, pvalue1 = ConditionalDcorr().test(x, y, z)
-        stat2 = ConditionalDcorr().statistic(x, y, z)
+        stat1, pvalue1 = ConditionalDcorr(bandwidth=bandwidth).test(x, y, z)
+        stat2 = ConditionalDcorr(bandwidth=bandwidth).statistic(x, y, z)
 
         assert_almost_equal(stat1, obs_stat, decimal=2)
         assert_almost_equal(stat2, obs_stat, decimal=2)
 
-    @pytest.mark.parametrize("n", [100, 200])
+    @pytest.mark.parametrize("n", [50, 100])
     def test_rep(self, n):
         x, y, z = indep_normal(n, 1)
         stat1, pvalue1 = ConditionalDcorr().test(x, y, z, random_state=2)
@@ -26,6 +27,23 @@ class TestCDcorrStat:
 
         assert stat1 == stat2
         assert pvalue1 == pvalue2
+
+    @pytest.mark.parametrize("n", [100])
+    @pytest.mark.parametrize("obs_stat", [0.12926180100498708])
+    @pytest.mark.parametrize("obs_pvalue", [0.06393606393606394])
+    def test_indep_normal_corr(self, n, obs_stat, obs_pvalue):
+        np.random.seed(123456789)
+        x, y, z = indep_normal(n, 1)
+        stat1, pvalue1 = ConditionalDcorr(use_cov=False).test(x, y, z)
+
+        assert_almost_equal(stat1, obs_stat, decimal=2)
+        assert_almost_equal(pvalue1, obs_pvalue, decimal=2)
+
+
+class TestCDcorrErrors:
+    def test_bandwith(self):
+        with pytest.raises(ValueError):
+            ConditionalDcorr(bandwidth="bad_bandwidth")
 
 
 class TestCDcorrTypeIError:
