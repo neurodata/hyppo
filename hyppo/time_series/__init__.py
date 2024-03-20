cf3b806e1fa53df0a8dd1fd9da8620f14d012e28
j1c <jaewonc78@gmail.com>
2024-03-19 20:29:42 -0500

    add init

--- hyppo/time_series/__init__.py
+++ hyppo/time_series/__init__.py
@@ -2,5 +2,6 @@ from .corrx import LjungBox
 from .dcorrx import DcorrX
 from .mgcx import MGCX
 from .hsicx import HsicX
+from .cca import CCAX
 
 __all__ = [s for s in dir()]  # add imported tests to __all__
