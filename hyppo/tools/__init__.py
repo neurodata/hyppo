b2fd0dc39032d2745bcdafa054adcb14abb3e729
Eric W. Bridgeford <ericwb95@gmail.com>
2025-03-05 23:01:52 -0800

    updated requirements and imports

--- hyppo/tools/__init__.py
+++ hyppo/tools/__init__.py
@@ -4,5 +4,8 @@ from .indep_sim import *
 from .ksample_sim import *
 from .power import *
 from .time_series_sim import *
+from .vm import VectorMatch, _CleanInputsVM
+from .cate_sim import *
+
 
 __all__ = [s for s in dir() if not s.startswith("_")]  # remove dunders
